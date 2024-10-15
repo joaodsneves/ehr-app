8579704c091556c613db5a693a4662877aa451ab
joaodsneves <joaocardosoneves98@gmail.com>
2024-10-15 10:09:23 -0300

    Retificando Configuração Anterior - Configuração Inicial do Frontend React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+
 
 function App() {
-  // Estado para armazenar os pacientes
-  const [pacientes, setPacientes] = useState([]);
-
-  // Função para buscar pacientes do backend
-  const fetchPacientes = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/pacientes');
-      setPacientes(response.data);
-    } catch (error) {
-      console.error('Erro ao buscar pacientes:', error);
-    }
-  };
-
-  // useEffect para buscar os pacientes ao carregar o componente
-  useEffect(() => {
-    fetchPacientes();
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Pacientes</h1>
-      <ul>
-        {pacientes.map((paciente) => (
-          <li key={paciente.id}>
-            {paciente.nome} - {paciente.idade} anos - {paciente.endereco}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <h1>Meu Blog</h1>
   );
 }
 
